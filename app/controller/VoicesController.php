eb086048e1018ee95508b8cff8ff0b1e0b01066b
左光明 <12949386+zgm2003@user.noreply.gitee.com>
2025-04-24 13:05:27 +0800

    ok

--- app/controller/VoicesController.php
+++ app/controller/VoicesController.php
@@ -6,6 +6,7 @@ use app\lib\AliCloud\TTS;
 use app\module\VoicesModule;
 use app\dep\VoicesDep;
 use support\Request;
+use support\Response;
 
 class VoicesController extends Controller{
 
@@ -20,18 +21,23 @@ class VoicesController extends Controller{
         $this->run([VoicesModule::class,'del'],$request);
         return $this->response();
     }
+
     public function listen(Request $request)
     {
         $param = $request->all();
         $voicesDep = new VoicesDep();
         $resDep = $voicesDep->first($param['id']);
+
         $text = '这是一段测试音频';
         $sampleRate = VoicesEnum::$hzArr[$resDep->sampling_rates];
-        $result = TTS::TssSync($text,$sampleRate,$resDep->code);
-        return $result;
+
+        $sdk = new TTS();
+        $audioBinary = $sdk->TssSync($text, $sampleRate, $resDep->code);
+
+        return response($audioBinary)
+            ->withHeader('Content-Type', 'audio/wav')
+            ->withHeader('Content-Disposition', 'inline; filename="speech.wav"');
     }
-
-
 
     public function list(Request $request){
         $this->run([VoicesModule::class,'list'],$request);
