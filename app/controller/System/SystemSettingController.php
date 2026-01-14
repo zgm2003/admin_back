64b7d98c3c9c4849abc04b6c56cffb022fa90b2d
左光明 <2093146753@qq.com>
2026-01-14 22:25:53 +0800

    feat(SystemSetting): 为系统设置控制器添加权限和操作日志注解
    - 为add方法添加OperationLog和Permission注解
    - 为edit方法添加OperationLog和Permission注解
    - 为del方法添加OperationLog和Permission注解
    - 为status方法添加OperationLog和Permission注解
    - 移除clearCache方法

--- app/controller/System/SystemSettingController.php
+++ app/controller/System/SystemSettingController.php
@@ -20,33 +20,43 @@ class SystemSettingController extends Controller
         return $this->response();
     }
 
+    /**
+     * @OperationLog("系统设置新增")
+     * @Permission("systemSetting.add")
+     */
     public function add(Request $request)
     {
         $this->run([SystemSettingModule::class, 'add'], $request);
         return $this->response();
     }
 
+    /**
+     * @OperationLog("系统设置编辑")
+     * @Permission("systemSetting.edit")
+     */
     public function edit(Request $request)
     {
         $this->run([SystemSettingModule::class, 'edit'], $request);
         return $this->response();
     }
 
+    /**
+     * @OperationLog("系统设置删除")
+     * @Permission("systemSetting.del")
+     */
     public function del(Request $request)
     {
         $this->run([SystemSettingModule::class, 'del'], $request);
         return $this->response();
     }
 
+    /**
+     * @OperationLog("系统设置状态变更")
+     * @Permission("systemSetting.status")
+     */
     public function status(Request $request)
     {
         $this->run([SystemSettingModule::class, 'status'], $request);
-        return $this->response();
-    }
-
-    public function clearCache(Request $request)
-    {
-        $this->run([SystemSettingModule::class, 'clearCache'], $request);
         return $this->response();
     }
 }
